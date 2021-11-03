9a3838df61e1e21e132216d42fb621a0c7a0449a
Lynk <dan.cerardi17@gmail.com>
2021-11-03 21:28:32 +0100

    Started working on Movement, CharacterController2D

---
+++ TrickShot_Unity_1.01/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    CharacterController2D controller;
+
+    float horizontalMove = 0f;
+
+    public float runSpeed = 40f;
+
+    void Start()
+    {
+       
+    }
+
+    
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal") * runSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+
+}
